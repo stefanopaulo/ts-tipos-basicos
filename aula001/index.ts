3c4c1719b2ea7a7fb555cfc9ba4f4f6462db9aaf
Stefano Paulo <stefanopaulo47@gmail.com>
2021-04-22 20:35:22 -0300

    sintaxe do type annotation no typescript

--- aula001/index.ts
+++ aula001/index.ts
@@ -1,1 +1,23 @@
-console.log("ola");
+// Tipos básicos (aqui ocorre inferência de tipos)
+let nome: string = 'Luiz';
+let idade: number = 56;
+let adulto: boolean = true;
+let simbolo: symbol = Symbol('qualquer-symbol');
+// let big: bigint = 10n;
+
+// Arrays
+let arrayDeNumeros: Array<number> = [1, 2, 3];
+let arrayDeLetras: string[] = ['a', 'b', 'c'];
+
+// Objetos
+let pessoa: { nome: string, idade: number, adulto?: boolean } = {
+    nome: 'Luiz',
+    idade: 30,
+};
+
+// Funções
+function soma(x: number, y: number) {
+    return x + y;
+}
+
+const soma2: (x: number, y: number) => number = (x, y) => x + y;
